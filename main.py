81e7c9d9d4ad4dc14e9eea2e97ac5ff886f5ea39
matsvl <90184009+matsvl@users.noreply.github.com>
2022-10-17 15:34:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def aantal_a_t_mutaties(file_name):
             try:
                 if line[3] == 'A' and line[4] == 'T':  # Checking if 'A' and 'T' are in the same line
                     mutation_count += 1  # Adding 1 to counter
-            except ValueError:
-                print("Value found not corresponding with intended value.")
+            except ValueError:  # Intercepting error for not corresponding values
+                print("Value found not corresponding with intended value.")  # Printing occurred problem
 
         print("The number of counted 'A' to 'T' mutations is:", mutation_count)  # Printing outcome
 
@@ -37,7 +37,7 @@ def aantal_a_t_mutaties(file_name):
               "check file name.")  # Printing occurred error
     except IOError:  # Intercepting error if file is not readable
         print("Inputted file could not be read.")  # Printing occurred error
-        
+
 
 def quality_lager_dan(file_name, quality=35):
     try:
@@ -50,13 +50,13 @@ def quality_lager_dan(file_name, quality=35):
             try:
                 if int(line[5]) < quality:  # Checking if value is lower than inputted value
                     quality_low_35_count += 1  # Adding 1 to counter
-            except ValueError:
+            except ValueError:  # Intercepting error for not corresponding values
                 print("Called on value does not qualify as integer (column headings).")  # Printing occurred error
                 exc_count += 1  # Adding 1 to counter
                 if exc_count > 1:  # Checking if counter is bigger than 1
                     print("Number of occurred ValueErrors is higher than expected, check for strings in column 'QUAL'.")  # Printing the occurred problem
 
-        print("The number of counted point mutations is:", quality_low_35_count)
+        print("The number of counted point mutations is:", quality_low_35_count)  # Printing outcome
 
     except FileNotFoundError:  # Intercepting error if file is not found
         print("Inputted file not corresponding with expected item, please check file name.")  # Printing occurred error
@@ -75,8 +75,8 @@ def unieke_filters(file_name):
             try:
                 if line[6] not in unique_ls:  # Checking if item is not in list
                     unique_ls.append(line[6])  # Adding item to list
-            except ValueError:
-                print("Value found not corresponding with intended value.")
+            except ValueError:  # Intercepting error for not corresponding values
+                print("Value found not corresponding with intended value.")  # Printing occurred problem
 
         return unique_ls  # Returning value to main
 
